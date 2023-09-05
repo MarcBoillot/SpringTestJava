658d6fae891e67996cf72d442c47248183f9a817
MarcBoillot <marc.boillot@le-campus-numerique.fr>
2023-09-05 13:39:47 +0200

    demoSpringJava

---
+++ helloworld/src/main/java/com/example/App.java
@@ -0,0 +1,14 @@
+package com.example;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
